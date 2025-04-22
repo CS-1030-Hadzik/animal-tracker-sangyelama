1c95fbfdbe6cf5c2f46935affd3abec98f482d8d
sangyelama <gamerzmoody@gmail.com>
2025-04-22 01:00:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,28 @@
 
+from animal import Animal
+from dog import Dog
+
 if __name__ == "__main__":
-    # TODO: Create an instance of the Animal class
-    # TODO: Print the Animal instance
-    # TODO: Call the method to make a generic animal sound
+    # Create an instance of the Animal class
+    animal1 = Animal("Simba", "Lion")
 
-    # TODO: Create an instance of the Dog class
-    # TODO: Print the Dog instance
-    # TODO: Call the method to make the dog-specific sound
+    # Print the Animal instance
+    print(animal1)
 
-    # TODO print out all the animals
+    # Call the method to make a generic animal sound
+    print(animal1.speak())
+
+    # Create an instance of the Dog class
+    dog1 = Dog("Max", "Canine", "Labrador")
+
+    # Print the Dog instance
+    print(dog1)
+
+    # Call the method to make the dog-specific sound
+    print(dog1.speak())
+
+    # Print out all the animals
+    print("\nAll Animals:")
+    for animal in Animal.all_animals:
+        print(animal)
+
