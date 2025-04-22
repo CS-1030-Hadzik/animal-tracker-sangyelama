2f695d8042bb44760f9ac432e2a38f061f573fa7
sangyelama <gamerzmoody@gmail.com>
2025-04-22 01:03:16 -0600

    Update animal.py

--- animal.py
+++ animal.py
@@ -2,16 +2,20 @@ class Animal:
     """
     Base class representing a generic animal.
     """
+
     # Class-level attribute
     kingdom = "Animalia"
-    # TODO create a class-level attribute that is a list of all the Animal objects
 
-    # TODO create the initializer for Animal with name and species as attributs
+    # Class-level list to store all Animal objects
+    all_animals = []
 
-    # TODO: Add a method to make a generic sound 
-    # Call the method `speak` and make it output a specific message like 
-    # "The animal makes a noise.""
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+        Animal.all_animals.append(self)
 
-    # TODO __str__ method for string representation
-    # Example output
-    # Kingdom: 'kingdom attribute', Name: 'name attribute' Species: 'species attribute' 
+    def speak(self):
+        return "The animal makes a noise."
+
+    def __str__(self):
+        return f"Kingdom: {self.kingdom}, Name: {self.name}, Species: {self.species}"
